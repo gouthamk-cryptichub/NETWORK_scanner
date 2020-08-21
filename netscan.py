4c1409aca8c1165ca8fe3995550fe717a9272d2c
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-08-21 08:15:55 -0400

    User friendly o/p

--- netscan.py
+++ netscan.py
@@ -5,7 +5,11 @@ def netscan(ip):
     arp_req = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_broad_req = broadcast/arp_req
-    live, dead = scapy.srp(arp_broad_req,timeout=1)
-    print(live.summary())
+    live = scapy.srp(arp_broad_req,timeout=1)[0]
 
+    print("==========================================")
+    print("IP ADDRESS \t\t  MAC Address")
+    print("==========================================")
+    for result in live:
+        print(" " + result[1].psrc + "    ------>    " + result[1].hwsrc)
 netscan("10.0.2.1/24")
