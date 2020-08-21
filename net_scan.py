feae4f8fd6d6e2db8520f3b52725c3984f3a15cf
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-08-21 07:11:38 -0400

    Added Command line args

--- net_scan.py
+++ net_scan.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 import scapy.all as scapy
+import optparse
+
+def get_args():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--iprange", dest="ip", help="IP or IP range of the target subnet.")
+    (value, args) = parser.parse_args()
+    if value.ip:
+        return value
+    else:
+        print("[-] ERROR Missing target IP, use --help for more info.")
+        exit()
 
 def netscan(ip):
-    scapy.arping(ip)
+        scapy.arping(ip)
 
-netscan("10.0.2.1/24")
+value = get_args()
+netscan(value.ip)
